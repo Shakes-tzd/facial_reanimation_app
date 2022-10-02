a210350bbb6dec815f9bf2a0412015ddbd575fd7
Shakes-tzd <tzd@mit.edu>
2022-10-02 03:19:38 -0400

    Update Abstractexplore.py

--- Abstractexplore.py
+++ Abstractexplore.py
@@ -200,15 +200,15 @@ try:
 </head>
 <!-- Customize page layout style according to your need and PDF file for best viewing experience -->
 <body style="margin: 100px 0 0 200px;">
-    <div id="adobe-dc-view" style="height: 360px; width: 500px;"></div>
+   <div id="adobe-dc-view"></div>
 <script src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
 <script type="text/javascript">
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
 		var adobeDCView = new AdobeDC.View({clientId: "ec53503d261f40cbb2f99bfd276b21d2", divId: "adobe-dc-view"});
 		adobeDCView.previewFile({
-            content:{location: {url: '"""+box+"""'}},
-            metaData:{fileName: '"""+fname+"""'}
-		}, {embedMode: "SIZED_CONTAINER"});
+			content:{promise: """+ box + """},
+			metaData:{fileName:  """+fname+ """}
+		}, {});
 	});
 </script>
 </body>
